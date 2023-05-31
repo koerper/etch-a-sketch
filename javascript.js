42cd9e806ece3fd3d86af7a29fe617dba1fbae3c
koerper <wuerger90@gmail.com>
2023-05-31 14:51:51 +0200

    Add option for resolution

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
 const canvas = document.querySelector('.canvas');
 
+
+
 function generateGrid (numElements,size) {
     for (let i=0; i<numElements; i++) {
         const row = document.createElement('div');
@@ -17,25 +19,46 @@ function generateGrid (numElements,size) {
     }
 }
 
-generateGrid(16,500);
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
 
-const pixels = document.querySelectorAll('.pixel');
-pixels.forEach(pixel => {
-    pixel.addEventListener('mouseover', function (e) {
-        if(e.buttons == 1 || e.buttons == 3){
-            this.classList.add('hovered');
-            // pixel.style.backgroundColor = 'black';
-        }
+function colorPixel (state) {
+    const pixels = document.querySelectorAll('.pixel');
+    pixels.forEach(pixel => {
+        pixel.addEventListener('mouseover', function (e) {
+            if(e.buttons == 1 || e.buttons == 3){
+                switch (state) {
+                    case 'rainbow':
+                        randomColor = Math.floor(Math.random()*16777215).toString(16);
+                        this.style.backgroundColor = "#" + randomColor;
+                        break;
+                    case 'shade':
+                        break;
+                    case 'eraser':
+                        break;
+                    default:
+                        this.classList.add('hovered');
+                        break;
+                }
+            }
+        })
     })
+}
+
+const slider = document.getElementById("resolutionSlider");
+const output = document.getElementById("resolutionOutput");
+output.innerHTML = `${slider.value} &times ${slider.value}`; // Display the default slider value
+
+
+slider.addEventListener("input", function () {
+    output.innerHTML = `${this.value} &times ${this.value}`;
+    removeAllChildNodes(canvas);
+    generateGrid(slider.value,500)
+    colorPixel('rainbow');
 })
 
-// const slider = document.getElementById("resolutionSlider");
-// const output = document.getElementById("resolutionOutput");
-// output.innerHTML = `${slider.value} &times ${slider.value}`; // Display the default slider value
-
-
-// slider.addEventListener("change", function () {
-//     output.innerHTML = `${this.value} &times ${this.value}`;
-//     // canvas.forEach(pixel => pixel.remove());
-//     generateGrid(slider.value,500)
-// })
+generateGrid(slider.value,500);
+colorPixel();
